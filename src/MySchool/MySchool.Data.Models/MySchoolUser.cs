a00e481feb392ace4b7fd28a5cbb951bff73587b
krisi kostadinov <krisi.199898@gmail.com>
2019-08-30 11:54:16 +0300

    Scaffolded and configured identity

---
+++ src/MySchool/MySchool.Data.Models/MySchoolUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+
+namespace MySchool.Data.Models
+{
+    public class MySchoolUser : IdentityUser<string>
+    {
+    }
+}
---
+++ src/MySchool/MySchool.Data/MySchoolContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MySchool.Data.Models;
+
+namespace MySchool.Data
+{
+    public class MySchoolContext : IdentityDbContext<MySchoolUser, IdentityRole<string>, string>
+    {
+        public MySchoolContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
